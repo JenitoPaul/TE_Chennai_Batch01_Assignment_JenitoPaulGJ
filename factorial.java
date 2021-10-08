126fbb03575593e07095288cf933260f600a3439
JenitoPaul <91727546+JenitoPaul@users.noreply.github.com>
2021-10-08 18:01:19 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,12 @@
+class factorial{
+	public static void main(String [] args)	{
+		for(int j=1;j<=10;j++){
+		int fact=1;
+		for(int i=1;i<j;i++){
+		fact+=(fact*i);
+		//System.out.println(fact);
+		}
+		System.out.println(j +" factorial is " +fact);
+		}
+	}
+}
